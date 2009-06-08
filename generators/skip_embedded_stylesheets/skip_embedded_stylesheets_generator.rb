d062148d4eff3f1b41830823393fd316e84951b1
mat_aki <matsumura.aki@gmail.com>
2009-06-08 17:15:29 +0900

    change direcotry of stylesheet to skip_embedded remove css file

--- generators/skip_embedded_stylesheets/skip_embedded_stylesheets_generator.rb
+++ generators/skip_embedded_stylesheets/skip_embedded_stylesheets_generator.rb
@@ -2,18 +2,13 @@ class SkipEmbeddedStylesheetsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       assets = %w[
-        public/stylesheets/skip_base.css
-        public/stylesheets/skip_embedded.css
-        public/stylesheets/skip_footer.css
-        public/stylesheets/skip_header.css
-        public/stylesheets/editor_area.css
-        public/stylesheets/sass/skip_base.sass
-        public/stylesheets/sass/skip_embedded.sass
-        public/stylesheets/sass/skip_footer.sass
-        public/stylesheets/sass/skip_header.sass
-        public/stylesheets/sass/editor_area.sass
-        public/stylesheets/sprites/sprites.css
-        public/stylesheets/sprites/sprites.png
+        public/stylesheets/sass/skip_embedded/skip_base.sass
+        public/stylesheets/sass/skip_embedded/skip_embedded.sass
+        public/stylesheets/sass/skip_embedded/skip_footer.sass
+        public/stylesheets/sass/skip_embedded/skip_header.sass
+        public/stylesheets/sass/skip_embedded/editor_area.sass
+        public/stylesheets/skip_embedded/sprites.css
+        public/stylesheets/skip_embedded/sprites.png
       ]
 
       assets.map{|asset| File.dirname(asset) }.uniq.each{|asset| m.directory(asset) }
--- lib/skip_embedded/helpers.rb
+++ lib/skip_embedded/helpers.rb
@@ -9,7 +9,8 @@ module SkipEmbedded
       ActionView::Helpers::AssetTagHelper.register_javascript_expansion :skip_fckeditor => %w[fckeditor/fckeditor skip_fckeditor_config]
 
       # TODO skip/style などをどの程度共通化するかを検討、それはではskip_embeddedという別名で
-      ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :skip_embedded => %w[skip_embedded sprites/sprites.css]
+      ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :skip_embedded => %w[skip_embedded/skip_embedded]
+      ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :skip_embedded_sprites => %w[skip_embedded/sprites]
     end
   end
 end
