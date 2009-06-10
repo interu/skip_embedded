2f17af75eb7c7f3b327edf103217a9c8e626457c
maedana <maeda.na@gmail.com>
2009-06-10 19:49:02 +0900

    [xxxx] modify user_data_format and group_data_format

--- lib/skip_embedded/rp_service/util.rb
+++ lib/skip_embedded/rp_service/util.rb
@@ -1,14 +1,14 @@
 module SkipEmbedded
   module RpService
     module Util
-      def user_data(identity_url, name, display_name, admin = false)
+      def user_data(identity_url, name, display_name, admin = false, delete = false)
         {:identity_url => identity_url, :name => name,
-          :display_name => display_name, :admin => admin}.reject{|k,v| v.nil? }
+          :display_name => display_name, :admin => admin, :delete? => delete}.reject{|k,v| v.nil? }
       end
 
-      def group_data(gid, name, display_name, members)
+      def group_data(gid, name, display_name, members, delete = false)
         {:gid => gid, :name => name, :display_name => display_name,
-          :members => members}.reject{|k,v| v.nil? }
+          :members => members, :delete? => delete}.reject{|k,v| v.nil? }
       end
       module_function :user_data, :group_data
     end
